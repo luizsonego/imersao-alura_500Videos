0cf94024e9308220dfcc12092b188aaa6022f587
Luiz Yavorski <leesonego@gmail.com>
2020-07-28 21:57:06 -0300

    slider

--- src/components/Carousel/partials/Slider/index.js
+++ src/components/Carousel/partials/Slider/index.js
@@ -42,7 +42,7 @@ const Slider = ({ children }) => (
   <Container>
     <SlickSlider {...{
       dots: false,
-      infinite: false,
+      infinite: true,
       speed: 300,
       centerMode: false,
       variableWidth: true,
@@ -55,3 +55,5 @@ const Slider = ({ children }) => (
 );
 
 export default Slider; 
+
+
